0c30a85775debf906081e8360ddb4d0fd2026770
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-11-10 13:07:40 -0500

    added schema and seeds

---
+++ config/seeds.sql
@@ -0,0 +1,3 @@
+use beer_tinder;
+
+INSERT INTO users (username, password, first_name, last_name, age, email_address) VALUES ('LaurenSantos23', 'password1', 'Lauren', 'Santosuosso', 33, 'Lauren.Santosuosso@yahoo.com');
