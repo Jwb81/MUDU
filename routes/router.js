25eafa2cd2454c4ac0431cdb7fde09e04ab7e904
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-11-12 23:33:20 -0500

    Added some get, put, and post, and delete methods to call in the routes and show the beers information from api

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+var connection = require("../config/connection.js");
 const orm = require('../config/orm');
 
 router.get('/', (req, res) => {
@@ -8,6 +8,39 @@ router.get('/', (req, res) => {
         success: true
     })
 })
+
+//get a list of beers form the db
+router.get('/beers', (req, res) => {
+    res.json(response).catch(err => {
+        res.json(err);
+        res.send({type: 'GET'});
+    })
+})
+
+//add new beer into the db
+router.post('/beers', (req, res) => {
+    res.json(response).catch(err => {
+        res.json(err);
+        res.send({type: 'POST'});
+    })
+})
+
+//update a new beers to the db
+router.put('/beers/:beerId/name/:name/', (req, res) => {
+    res.json(response).catch(err => {
+        res.json(err);
+        res.send({type: 'PUT'});
+    })
+})
+
+//delete a beer from the db
+router.delete('/beers/:beerId/name/:name', (req, res) => {
+    res.json(response).catch(err => {
+        res.json(err);
+        res.send({type: 'DELETE'});
+    })
+})
+
 
 router.get('/all-drinking-buddies', (req, res) => {
     orm.getAllDrinkingBuddies()
