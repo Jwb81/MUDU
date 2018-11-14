331132f66d3296c237845d6c14c53fbd41076f53
Jason Barnett <jayb21@neo.rr.com>
2018-11-13 19:21:16 -0500

    added a test command to test requests on the server

--- routes/router.js
+++ routes/router.js
@@ -20,4 +20,11 @@ router.get('/all-drinking-buddies', (req, res) => {
         })
 })
 
+router.get('/beer', (req, res) => {
+    request('https://sandbox-api.brewerydb.com/v2/beers?key=d00fe48488b9bc5528a4f5aab7f5c4ed', (err, response, body) => {
+        res.send(body.currentPage);
+        // console.log(body);
+    })
+})
+
 module.exports = router; 
