a1d3b7abf595c9bf6a653153135d7f2ad12f37c2
Jason Barnett <jayb21@neo.rr.com>
2018-11-15 19:42:23 -0500

    default route returns index page

--- routes/router.js
+++ routes/router.js
@@ -4,13 +4,75 @@ const router = express.Router();
 var connection = require("../config/connection.js");
 const orm = require('../config/orm');
 
-// read in all beers from api
+/**
+ * 1. make a request to get the number of pages
+ * 2. make a request for each page and add all drinks to an object hosted on the server
+ * 3. filter over that array whenever needed
+ */
+const breweryDbURL = 'https://sandbox-api.brewerydb.com/v2/beers?key=d00fe48488b9bc5528a4f5aab7f5c4ed&withBreweries=Y';
+let pages;
+let allBeers = [];
+
+const sleep = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    })
+}
+const getBeersFromAPI = new Promise((resolve, reject) => {
+    request(breweryDbURL, (err, response, body) => {
+        if (err) {
+            return reject(err);
+        }
+        body = JSON.parse(body);
+        pages = body.numberOfPages;
+        // console.log(pages);
+
+        let promiseArray = [];
+
+        // for (let i = 1; i <= pages; i++) {
+        for (let i = 1; i <= 10; i++) {
+            if (pages % 10 === 0) {
+                // sleep(1100);
+            } 
+            promiseArray = promiseArray.concat(new Promise((resolve, reject) => {
+                request(`${breweryDbURL}&p=${i}`, (err, response, body) => {
+                    // console.log(i);
+                    if (err) {
+                        return reject(err);
+                    }
+                    body = JSON.parse(body);
+                    if (i === 1) {
+                    }
+                    // console.log(body.data)
+                    resolve(body.data);
+                })
+            }))
+        }
+
+        Promise.all(promiseArray).then(values => {
+            // console.log(values);
+            for (let i = 0; i < values.length; i++) {
+                allBeers = allBeers.concat(values[i]);
+            }
+            resolve(allBeers);
+            // console.log(allBeers.length);
+        })
+    })
+})
+.then(result => {
+    console.log(result.length);
+}) 
+.catch(err => {
+    console.log(err);
+})
+
 
 
 router.get('/', (req, res) => {
-    res.json({
-        success: true
-    })
+    res.sendFile('./index.html');
+    // res.json({
+    //     success: true
+    // })
 })
 
 router.get('/all-drinking-buddies/:username', (req, res) => {
@@ -34,8 +96,8 @@ router.get('/unmatched-beers/:username', (req, res) => {
 
 router.get('/allbeers/:pageNumber', (req, res) => {
     const page = req.params.pageNumber || 1;
-    request(`https://sandbox-api.brewerydb.com/v2/beers?key=d00fe48488b9bc5528a4f5aab7f5c4ed&p=${page}`, (err, response, body) => {
-    // request(`https://sandbox-api.brewerydb.com/v2/beers?key=d00fe48488b9bc5528a4f5aab7f5c4ed`, (err, response, body) => {
+    request(`https://sandbox-api.brewerydb.com/v2/beers?key=d00fe48488b9bc5528a4f5aab7f5c4ed&p=${page}&withBreweries=Y`, (err, response, body) => {
+        // request(`https://sandbox-api.brewerydb.com/v2/beers?key=d00fe48488b9bc5528a4f5aab7f5c4ed`, (err, response, body) => {
         res.json(JSON.parse(response.body));
         // console.log(body);
     })
@@ -51,11 +113,11 @@ router.post('/beer-match', (req, res) => {
             res.sendStatus(200);
         })
         .catch(err => {
-            
+
         })
 
 
 
 })
 
-module.exports = router; 
+module.exports = router;
