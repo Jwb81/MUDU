bbcd1bb53be164d7adbb5dee31d6e27fa2b2a363
Jason Barnett <jayb21@neo.rr.com>
2018-11-17 11:11:28 -0500

    added an image for unavailable beer images and moved the setup file into an if statement so the server does not crash

--- config/seeds.sql
+++ config/seeds.sql
@@ -3,6 +3,6 @@ use beer_tinder;
 INSERT INTO drinking_buddies (username1, username2)
 VALUES ('Jason Barnett', 'some random dude');
 
-INSERT INTO users (username, password, first_name, last_name, age, email_address) 
+INSERT INTO users (username, password, first_name, last_name, age, address, email_address) 
 VALUES 
-('LaurenSantos23', 'password1', 'Lauren', 'Santosuosso', 33, 'Lauren.Santosuosso@yahoo.com');
+('LaurenSantos23', 'password1', 'Lauren', 'Santosuosso', 33, '426 Tyner Street Akron Ohio', 'Lauren.Santosuosso@yahoo.com');
